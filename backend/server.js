69e1839d26fa86ad8edb8fafdc26e2d3c3214cde
abhiram <abhiramsankarsetty@gmail.com>
2025-05-31 19:04:32 +0530

    again corrected cors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ const app=express()
 
 // Middleware
 app.use(cors({
-    origin: [process.env.CLIENT_URL, 'http://localhost:3001'],
-    credentials: true  // This is important for cookies
+    origin: [process.env.CLIENT_URL, 'http://localhost:3001', 'https://doubtroom.onrender.com'],
+    credentials: true,  // This is important for cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.use(express.json())
