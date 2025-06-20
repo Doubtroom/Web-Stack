9c3b30b26839ff2537f14db7bd435acda1d6b637
abhiram <abhiramsankarsetty@gmail.com>
2025-06-20 13:04:25 +0530

    fix(server): specify database name in MongoDB connection
    - Updated mongoose connection to include dbName option, setting it to "org-db" for clarity and proper database targeting.

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,9 @@ app.use(cookieParser())  // Add cookie-parser middleware
 app.use('/api/auth',authRoutes)
 app.use('/api/data',dataRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+  dbName: "org-db"
+})
 .then(()=>{
   app.listen(process.env.PORT || 5000,()=>{
     console.log(`Server is running on port ${process.env.PORT || 5000}`)
