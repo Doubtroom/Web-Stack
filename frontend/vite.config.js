edde248c7ba8bcd20f05109fea5ea7518fbd1777
abhiram <abhiramsankarsetty@gmail.com>
2025-06-27 19:57:33 +0530

    fix(frontend): update Vite configuration for HTTPS in development
    - Refactored the Vite configuration to conditionally set HTTPS options based on the environment.
    - Moved HTTPS key and certificate loading into a development-only block to improve security and flexibility.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
 
+let httpsConfig = undefined;
+if (process.env.NODE_ENV === 'development') {
+  httpsConfig = {
+    key: fs.readFileSync('./cert/localhost-key.pem'),
+    cert: fs.readFileSync('./cert/localhost.pem'),
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('./cert/localhost-key.pem'),
-      cert: fs.readFileSync('./cert/localhost.pem'),
-    },
+    https: httpsConfig,
     port: 3001,
   },
   build: {
