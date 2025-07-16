d8c9991d5630b68f75eb1b17d358f5fbccb1ef2f
abhiram <abhiramsankarsetty@gmail.com>
2025-07-17 00:12:08 +0530

    chore: simplify Vite configuration and enhance API error handling
    - Removed HTTPS configuration from Vite setup for local development to streamline the configuration process.
    - Updated API client error handling to conditionally redirect to the login page only in production mode, preventing unnecessary redirects during development.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 let serverConfig = {
   port: 3001,
 };
-
-// if (process.env.VITE_NODE_ENV === "development") {
-  try {
-    serverConfig.https = {
-      key: fs.readFileSync("./cert/localhost.key"),
-      cert: fs.readFileSync("./cert/localhost.crt"),
-    };
-  } catch (e) {
-    serverConfig.https = undefined;
-  }
-// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
