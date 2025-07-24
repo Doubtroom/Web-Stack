a068d410712d7df0fecebbdc1440e7a533cde188
Abhiram Sankarasetty <90583302+mrhat05@users.noreply.github.com>
2025-07-24 14:20:07 +0530

    chore: update dependencies and clean up configuration (#21) (#22)
    - Added @faker-js/faker dependency to package.json and package-lock.json for improved data generation capabilities.
    - Removed 'scripts/' entry from .gitignore to allow for better management of script files.
    - Simplified mongoose connection in server.js by removing the dbName option, streamlining the database connection process.

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,7 @@ app.use("/api/form-data", formDataRoutes);
 
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    dbName: "org-db",
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
